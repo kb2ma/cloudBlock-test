72dce259fd7ce2b24f936282646d627279e9e012
Ken Bannister <kb2ma@runbox.com>
2021-04-27 13:28:37 -0400

    Add device UUID to mock-data data_source
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- mock-data/data_source/main.py
+++ mock-data/data_source/main.py
@@ -1,6 +1,7 @@
 """
 Test app to push the current time to MQTT every 30 seconds.
 """
+import os
 import paho.mqtt.client as mqtt
 from datetime import datetime
 import time
@@ -8,10 +9,11 @@ import time
 client = mqtt.Client()
 while True:
     client.connect("localhost", 1883, 60)
-    now_str = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    data = "{}, {}".format(os.getenv('BALENA_DEVICE_UUID'),
+                        datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
 
     #print("Sending " + str(now))
-    msgInfo = client.publish('cloud-input', now_str, 0, False)
+    msgInfo = client.publish('cloud-input', data, 0, False)
     if False == msgInfo.is_published():
         msgInfo.wait_for_publish()
     client.disconnect()
